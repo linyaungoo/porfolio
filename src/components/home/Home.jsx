d3891eb81c8caa927269c4641f5d290d10eea423
linyaungoo <linyaungoo3199@gmail.com>
2023-06-03 16:43:11 +0630

    lyo

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import "./home.css";
+import Me from '../../assets/avatar-1.svg'
+import HeaderSocials from './HeaderSocials'
+import ScrollDown from './ScrollDown';
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Lin Yaung Oo</h1>
+        <span className="home__education">
+          I'm a Front-End Developer
+        </span>
+        <HeaderSocials/>
+        <a href="#contact" className='btn'>Hire me</a>
+        <ScrollDown/>
+      </div>
+    </section>
   )
 }
 
